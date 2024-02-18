e5a5c09806c7c426c7c6065c1b0c82e4e6c0bb99
jordana braga <danaxbraga@gmail.com>
2024-02-18 21:00:13 +0000

    Setting up books app

--- lovebooks/urls.py
+++ lovebooks/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from books import views as book_views
 
 urlpatterns = [
+    path('books/', book_views.book, name='book'),
     path('admin/', admin.site.urls),
 ]
