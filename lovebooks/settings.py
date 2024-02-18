75bcca1f13f47ed568f73feb5ba862cfe29380b5
jordana braga <danaxbraga@gmail.com>
2024-02-18 23:36:33 +0000

    Adding keys

--- lovebooks/settings.py
+++ lovebooks/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$4^@jrcb_fkbapg)yd5*-y0(-swdm9wo$0=rh=mr0x#)c#!_t('
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
