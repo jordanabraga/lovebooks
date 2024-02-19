417e1a89f60bf277adc271ee301f3098c38144d4
jordana braga <danaxbraga@gmail.com>
2024-02-19 02:08:20 +0000

    Adjust book model, delete author model and add autoslug

--- books/models.py
+++ books/models.py
@@ -1,23 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
+from autoslug import AutoSlugField
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
-# Create your models here.
-class Author(models.Model):
-    name = models.CharField(max_length=200, unique=True)
-
-    def __str__(self):
-        return self.name
-
+# Create your models here
 
 class Book(models.Model):
     title = models.CharField(max_length=200, unique=True)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
-    slug = models.SlugField(max_length=200, unique=True)
+    author = models.CharField(max_length=200, unique=False)
+    slug = AutoSlugField(populate_from='title', always_update=True)
     added_by = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="add_book"
     )
     summary = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
