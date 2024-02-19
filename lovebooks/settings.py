b2e5307786459bf90a0907b7a4fc4efb89d1b62d
jordana braga <danaxbraga@gmail.com>
2024-02-19 03:45:05 +0000

    Add json file with books

--- lovebooks/settings.py
+++ lovebooks/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-jordanabraga-lovebooks-yrdw8a4de4i.ws-eu108.gitpod.io',
                 '.herokuapp.com']
