ce73dbb16b3f1b09bbd428b3a94b204227166948
jordana braga <danaxbraga@gmail.com>
2024-02-19 02:55:17 +0000

    Adjust order of books on the listview

--- books/views.py
+++ books/views.py
@@ -4,5 +4,5 @@ from .models import Book
 
 # Create your views here.
 class BooksList(generic.ListView):
-    queryset = Book.objects.all()
+    queryset = Book.objects.all().order_by("-created_on")
     template_name = "books_list.html"
