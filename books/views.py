06c6c7d7c8b2a1eaaa23f2674549ad6fab8f5eef
jordana braga <danaxbraga@gmail.com>
2024-02-19 22:40:34 +0000

    Add base html

--- books/views.py
+++ books/views.py
@@ -5,4 +5,5 @@ from .models import Book
 # Create your views here.
 class BooksList(generic.ListView):
     queryset = Book.objects.all().order_by("-created_on")
-    template_name = "books_list.html"
+    template_name = "books/index.html"
+    paginate_by = 6
