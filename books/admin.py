c07f25220db8d974f59600f0d08bb9387d5e4c5e
jordana braga <danaxbraga@gmail.com>
2024-02-19 03:28:39 +0000

    Add summernote

--- books/admin.py
+++ books/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Book, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Book)
+class BookAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'author', 'slug', 'status')
+    search_fields = ['title', 'author']
+    list_filter = ('status', 'created_on')
+    summernote_fields = ('summary',)
+
 
 # Register your models here.
-admin.site.register(Book)
 admin.site.register(Comment)
