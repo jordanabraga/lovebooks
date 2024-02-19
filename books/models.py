8ee2057d38c92ace5402a1fd789e273ea3f765d9
jordana braga <danaxbraga@gmail.com>
2024-02-19 02:27:46 +0000

    Add comment model

--- books/models.py
+++ books/models.py
@@ -19,3 +19,24 @@ class Book(models.Model):
 
     def __str__(self):
         return f"{self.title} by {self.author}"
+
+class Comment(models.Model):
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    added_by = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="comments_user"
+    )
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.added_by}"
