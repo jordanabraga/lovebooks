60348397a5a1db400debb694c3fd26a25aa95b18
jordana braga <danaxbraga@gmail.com>
2024-02-19 01:32:02 +0000

    Create book model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
+    slug = models.SlugField(max_length=200, unique=True)
+    added_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="add_book"
+    )
+    summary = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
