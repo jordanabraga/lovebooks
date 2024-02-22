87bfa0da63d1788449482a31569037771bb45614
jordana braga <danaxbraga@gmail.com>
2024-02-22 05:28:43 +0000

    Add static file to lovebooks

--- books/static/js/comments.js
+++ books/static/js/comments.js
@@ -38,9 +38,9 @@ for (let button of editButtons) {
 * the user for confirmation before deletion.
 */
 for (let button of deleteButtons) {
-    button.addEventListener("click", (e) => {
-      let commentId = e.target.getAttribute("comment_id");
-      deleteConfirm.href = `delete_comment/${commentId}`;
-      deleteModal.show();
-    });
-  }
+  button.addEventListener("click", (e) => {
+    let commentId = e.target.getAttribute("comment_id");
+    deleteConfirm.href = `delete_comment/${commentId}`;
+    deleteModal.show();
+  });
+}
